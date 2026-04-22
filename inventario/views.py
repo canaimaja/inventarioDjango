f7df025c0f8be9106b1a17b39730cbb1225b8216
canaimaja <canaimaja@gmail.com>
2026-04-22 15:14:04 -0400

    avance parte 3.2

---
+++ inventario/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render, redirect 
+from django.contrib.auth.decorators import login_required 
+from .models import Producto 
+from .forms import ProductoForm 
+
+
+def lista_productos(request): 
+    productos = Producto.objects.all() 
+    return render(request, 'inventario/lista.html', {'productos': productos}) 
+
+
+
+@login_required  # Protección nativa (Pág. 9 )
+def crear_producto(request): 
+   if request.method == 'POST': 
+        form = ProductoForm(request.POST) 
+        if form.is_valid(): 
+            form.save() 
+            return redirect('lista_productos') 
+        else: 
+            form = ProductoForm() 
+        return render(request, 'inventario/formulario.html', {'form': form}) 
